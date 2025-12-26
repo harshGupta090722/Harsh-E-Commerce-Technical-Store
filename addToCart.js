1bb092c4a6568cfc9a98f5caa0219705d7f19e65
Harsh Gupta <harshgupta0907@gmail.com>
2025-12-26 23:50:07 +0530

    update products on localStorage

--- addToCart.js
+++ addToCart.js
@@ -5,7 +5,6 @@ import { updateCartValue } from "./updateCartValue";
 getCartProductFromLocalLS();
 
 export const addToCart=(event,id,stock)=>{
-    
     let arrLocalStorageProduct=getCartProductFromLocalLS();
     
     const currentProdElem=document.querySelector(`#card${id}`);
@@ -15,23 +14,33 @@ export const addToCart=(event,id,stock)=>{
     price=price.replace("₹","");
     
     
-    let existingProduct=arrLocalStorageProduct.find((currProd)=>
-        currProd.id === id
-);
-
-if(existingProduct){
+    let existingProduct=arrLocalStorageProduct.find((currProd)=>currProd.id === id);
     
-    return false;
-}
-
-price=Number(price*quantity);
-quantity=Number(quantity);
-console.log(price,quantity);
-
-
-let updatedCart={id,quantity,price};
-arrLocalStorageProduct.push(updatedCart);
-localStorage.setItem("cartProductLS",JSON.stringify(arrLocalStorageProduct));
-
-updateCartValue(arrLocalStorageProduct);
+    if(existingProduct && quantity>1){
+        quantity=Number(existingProduct.quantity) + Number(quantity);
+        price=Number(price*quantity);
+        
+        let updatedCart={id,quantity,price};
+        
+        updatedCart=arrLocalStorageProduct.map((currProd)=>{
+            return currProd.id===id?updatedCart:currProd;
+        });
+        
+        localStorage.setItem("cartProductLS",JSON.stringify(updatedCart));
+        alert("Selected already in the cart element !");
+    }
+    
+    if(existingProduct){
+        return false;
+    }
+    
+    price=Number(price*quantity);
+    quantity=Number(quantity);
+    
+    
+    let updatedCart={id,quantity,price};
+    arrLocalStorageProduct.push(updatedCart);
+    localStorage.setItem("cartProductLS",JSON.stringify(arrLocalStorageProduct));
+    
+    updateCartValue(arrLocalStorageProduct);
 }
--- updateCartValue.js
+++ updateCartValue.js
@@ -3,5 +3,3 @@ const cartValue=document.querySelector("#cartValue");
 export const updateCartValue=(cartProducts)=>{
     return (cartValue.innerHTML=`<i class="fa-solid fa-cart-shopping"> ${cartProducts.length} </i>`);
 };
-
-
